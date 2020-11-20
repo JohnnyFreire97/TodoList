415f324671f9a641670e016b39c9e977348e523d
Johnny Freire <johnnyfreirejh@hotmail.com>
2020-11-20 18:15:21 -0300

    Add style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ToDoList from './todolist';
-
+import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,13 +11,23 @@ class App extends React.Component {
     handleChange = (e) =>{
       this.setState({text:e.target.value})
     }
+    
+    handleRemove = (id) => {
+      var list=this.state.itens.filter(function (el){
+              return el.id !== id;
+          } )
+          this.setState(
+              state=>({itens:list})
+            );
+     
+      }
   
     handleSubmit = (e) => {
       e.preventDefault();
       if(!this.state.text.length){
         return;
       }
-  
+      
       const item = {
         text:this.state.text,
         id: Date.now()
@@ -28,19 +38,21 @@ class App extends React.Component {
   
     }
 
+
+
   render() {
     return (
-      <div>
-        <h3>TAREFAS</h3>
+      <div className='todo-app'>
+        <h1>To do List</h1>
        
         <br/>
-        <form onSubmit={this.handleSubmit}>
-          <input id="newToDo" onChange={this.handleChange}
+        <form className='todo-form' onSubmit={this.handleSubmit}>
+          <input className='todo-input' id="newToDo" onChange={this.handleChange}
           value={this.state.text}/>
-          <button>Inserir</button>
+          <button className='todo-button'>Inserir</button>
         </form>
         <br/>
-        <ToDoList itens = {this.state.itens}/>
+        <ToDoList itens = {this.state.itens}  fooDelete={this.handleRemove}/>
       </div>
     );
   }
--- src/todolist.js
+++ src/todolist.js
@@ -1,14 +1,25 @@
 import React from 'react';
-
-class ToDoList extends React.Component{
-    constructor(props){
+import { RiCloseCircleLine } from 'react-icons/ri';
+import './App.css';
+class ToDoList extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(
+
+
+
+    render() {
+        return (
             <ul>{this.props.itens.map(
-            item=>(<li key={item.id}>{item.text}</li>)
+                item => (
+                    <li className="todo-row" key={item.id}>{item.text}
+                        <div className="icons">
+                            <RiCloseCircleLine className='delete-icon' 
+                            onClick={() => this.props.fooDelete(item.id)} />
+                        </div>
+                    </li>
+                )
             )}</ul>
         );
     }
